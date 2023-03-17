992ee222dba1e26b0050479cd61a0fa3aca22481
Jay Savaliya <jrsavaliya04@gmail.com>
2023-03-18 02:47:19 +0530

    Tutor Course Page 2

--- src/Pages/Course.jsx
+++ src/Pages/Course.jsx
@@ -1,86 +1,41 @@
-import React from 'react'
-import { valiCourse } from './Validation/valiCourse';
-import { Form, Formik } from "formik";
-import CustomInput from "../components/CustomInput";
-import CustomUpload from "../components/CustomUpload";
-
-
-const onSubmit = async (values, actions) => {
-  console.log(values);
-
-  await new Promise((resolve) => setTimeout(resolve, 1000));
-  actions.resetForm();
-};
+import React, { useState } from "react";
+// import { useSubmit } from "react-router-dom";
+import Tutor_Course from "./Tutor/Tutor_Course";
 
 const Course = () => {
+  const [shown, setshown] = useState(false);
+  const [updtxt, setupdtxt] = useState("Add A Course");
+  const handleShow = (event) => {
+    setshown((current) => !current);
+    if(shown===true){
+      setupdtxt("Add A Course");
+    }else{
+      setupdtxt("Close This Course Section");
+    }
+  };
   return (
-
     <>
-      <div className="mt-10">
-        <section className="max-w-4xl p-6 mx-auto rounded-md shadow hover:shadow-lg">
-          <h1 className="text-xl font-bold capitalize dark: text-center mb-1">Add Course Here!</h1>
-          <h2 className="text-xl capitalize dark: text-center mb-5">Enter Details of Course</h2>
-          <hr />
-
-          <Formik
-            initialValues={{ title: "",image: "",priceCourse: "", tutorName: ""}}
-            validationSchema={valiCourse}
-            onSubmit={onSubmit}
-          >
-            {({ isSubmitting }) => (
-              <Form >
-
-                <div className="grid">
-
-
-                </div>
-                <div className="grid grid-cols-1 gap-6 mt-4 sm:grid-cols-1">
-
-                  <div>
-                    {/* <label htmlFor="username">Username</label> */}
-
-                    <CustomInput
-                      label="title"
-                      name="title"
-                      type="text"
-                      placeholder="Enter your title"
-                    />
-                  </div>
-                  <div>
-                    <CustomUpload
-                      label="Upload Thumbnail"
-                      type="file"
-                      name="image"
-                    />
-                  </div>
-                  <div>
-                    <CustomInput
-                      label="Price"
-                      type="number"
-                      name="priceCourse"
-                    />
-                  </div>
-                  <div>
-                    <CustomInput
-                      label="Tutor Name"
-                      type="text"
-                      name="tutorName"
-                    />
-                  </div>
-                </div>
-                <hr className='mt-3' />
-                <div className="flex justify-center mt-6">
-                  <button disabled={isSubmitting} type="submit" className="px-3 py-2 leading-5 text-white transition-colors duration-200 transform bg-blue-500 rounded-md hover:bg-blue-700 focus:outline-none">Let's Go</button>
-                </div>
-              </Form>
-            )}
-          </Formik>
-        </section>
-
-
+      <div className="mt-10 ml-10">
+        <button
+          className="inline-flex items-center px-4 py-2 font-bold bg-indigo-700 hover:bg-indigo-800 text-white text-sm  rounded-md"
+          onClick={handleShow}
+        >
+          {updtxt && (
+            `${updtxt}`
+          )}
+          
+        </button>
       </div>
+      {shown && (
+        <div className="mt-10 ml-10">
+          {
+            // eslint-disable-next-line
+            <Tutor_Course />
+          }
+        </div>
+      )}
     </>
-  )
-}
+  );
+};
 
-export default Course
+export default Course;
